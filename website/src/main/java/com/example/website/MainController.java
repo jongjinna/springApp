6bb66b38b93d79228f8b07e33862a93c931bcb41
jongjinna <njjwa1004@naver.com>
2022-09-18 20:50:20 +0900

    :art: mysql ,,,

--- website/src/main/java/com/example/website/MainController.java
+++ website/src/main/java/com/example/website/MainController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
-    @RequestMapping("/hello")
+public class MainController {
+    @RequestMapping("/main")
     @ResponseBody
-    public String hello() {
-        return "Hello, IM JONGJIN na!";
+    public String index() {
+        return "<h1>Hi, I`m Jongjin.</h1></br> <h2>Nice to Meet you!</h2>";
     }
 }
