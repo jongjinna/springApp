57d19171cd118b8f8b1b3ab6441b8f3ac971e57c
jongjinna <njjwa1004@naver.com>
2022-10-27 20:04:28 +0900

    2022.10.27.

---
+++ tory/src/main/java/DOP/tory/MainController.java
@@ -0,0 +1,15 @@
+package DOP.tory;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/DOPTORY")
+    @ResponseBody
+    public String index() {
+        return "Hello, This is Dop site, i make this site to study spring";
+    }
+}
