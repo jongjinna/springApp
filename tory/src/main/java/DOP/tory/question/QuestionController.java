657589858893c953d7d73185bf4c414d75f82f4c
jongjinna <njjwa1004@naver.com>
2022-11-02 22:42:44 +0900

    jump to spring-boot ~(2-10)

--- tory/src/main/java/DOP/tory/question/QuestionController.java
+++ tory/src/main/java/DOP/tory/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,5 +20,11 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+    @RequestMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
