8e5fb9f307f201e33fe66b2a168c357d3223bbf3
jongjinna <njjwa1004@naver.com>
2022-11-13 17:23:49 +0900

    점프투스프링 3-3 완

--- tory/src/main/java/DOP/tory/question/QuestionService.java
+++ tory/src/main/java/DOP/tory/question/QuestionService.java
@@ -1,10 +1,15 @@
 package DOP.tory.question;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.time.LocalDateTime;
 import DOP.tory.DataNotFoundException;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -27,6 +32,14 @@ public class QuestionService {
             throw new DataNotFoundException("question not found");
         }
     }
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
+    }
+
     public void create(String subject, String content) {
         Question q = new Question();
         q.setSubject(subject);
